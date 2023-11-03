ce0921fb5621ee8c722e2f807aa9cabeacb9ddc5
Kenvbin <gixxer5506@yahoo.com>
2023-11-03 16:43:03 -0700

    6-0

---
+++ src/Penguin.java
@@ -0,0 +1,11 @@
+public class Penguin {
+    public void slide(){
+        System.out.println("The penguin slides on ice.");
+    }
+    public void swim(){
+        System.out.println("The penguin swims in the water.");
+    }
+    public void eat(){
+        System.out.println("The penguin eats fish.");
+    }
+}
